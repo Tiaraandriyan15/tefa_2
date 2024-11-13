6c306e2f63cfcfbd62c6f7ea28f63518419c9c5e
Tiaraandriyan15 <158125769+Tiaraandriyan15@users.noreply.github.com>
2024-11-13 09:37:47 +0700

    Add files via upload

---
+++ pages/login/index.vue
@@ -0,0 +1,54 @@
+    <template>
+    <div class="container-fluid d-flex justify-content-center align-items-center" style="background-color: #80A1E8;">
+            <div class="col-3">
+                <div class = "card shadow p-3 rounded-4" style="background-color: #D9D9D9;">
+                    <h1 class="text-center">Login MyAdmin</h1>
+                    <form @submit.prevent="loginKeApp">
+                        <label>email</label>
+                        <input v-model="email" type="email"  class="m-4"><br>
+                        <label>password</label>
+                        <input v-model="password" type="password" class="m-2"><br>                        
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                            <button class="btn btn-primary me-md-2" type="submit">LOGIN</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+    </div>
+</template>
+
+<script setup>
+const email = ref("")
+const password = ref("")
+const supabase = useSupabaseClient()
+
+async function loginKeApp() {
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: email.value,
+        password: password.value,
+    })
+    if(data) navigateTo('/')
+
+}
+
+</script>
+
+
+
+
+
+<style setup>
+    .container-fluid{
+        height: 100vh;
+    }
+
+    .card{
+        height: 300px;
+    }
+</style>
+
+<script scope>
+    definePageMeta({
+        layout: 'loginPage'
+    })
+</script>
